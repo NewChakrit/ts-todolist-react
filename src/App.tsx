8aa12e177523ffbc3f0a0da4f044c2811eca285c
chakrit tnasinpaibool <imnew108@gmail.com>
2022-06-11 22:15:13 +0700

    onsubmit func

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css';
 import InputFeild from './components/InputFeild';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Todo } from './modal';
 
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
 
   return (
     <div className="App">
     <span className="heading">Taskify</span>
-    <InputFeild todo={todo} setTodo={setTodo}/>
+    <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
