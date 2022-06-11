a23aa2e68c07e81fe9e1d83d97fc9c7e3682962a
chakrit tnasinpaibool <imnew108@gmail.com>
2022-06-11 22:01:51 +0700

    proptype

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { useState } from 'react';
 
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
+
   return (
     <div className="App">
     <span className="heading">Taskify</span>
-    <InputFeild/>
+    <InputFeild todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "./style.css"
 
-const InputFeild = () => {
-    const [todo, setTodo] = useState("")
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputFeild: React.FC<Props> = ({todo, setTodo}) => {
 
   return (
     <form className='input'>
-        <input type="input" placeholder='Enter a task' className='input-box' />
+        <input 
+        value={todo}
+        onChange={e=>setTodo(e.target.value)}
+        type="input"
+        placeholder='Enter a task' 
+        className='input-box' />
         <button className='input-submit' type='submit'>Go</button>
     </form>
   )
